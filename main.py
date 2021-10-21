411d664456f9f5f89c561947b1775d42f73c18bf
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-10-21 15:00:58 -0500

    Ball now spawns on click and despawns when it hits the bottom

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ PADDLE_STARTING_POS = ((WINDOW_WIDTH - PADDLE_WIDTH) / 2, WINDOW_HEIGHT - 40)
 PADDLE_COLOR = Colors.LIGHT_BLUE
 # Ball -------------------------------------------------------------------------
 BALL_SIZE = 10
-BALL_STARTING_POS = ((WINDOW_WIDTH - BALL_SIZE) / 2, PADDLE_STARTING_POS[1] - BALL_SIZE)
+BALL_STARTING_POS = ((WINDOW_WIDTH - BALL_SIZE) / 2, 350)
 BALL_STARTING_DIRECTION = 200
 BALL_SPEED = 7
 BALL_COLOR = Colors.WHITE
@@ -64,9 +64,8 @@ sprites = pygame.sprite.Group()
 paddle = Paddle(PADDLE_COLOR, PADDLE_WIDTH, PADDLE_HEIGHT, PADDLE_STARTING_POS)
 sprites.add(paddle)
 # Ball
-ball = Ball(BALL_COLOR, BALL_SIZE, BALL_STARTING_POS, BALL_STARTING_DIRECTION, BALL_SPEED,
-            min_y=PLAYFIELD_TOP_Y)
-sprites.add(ball)
+# Initialize empty, will be set to instance of Ball when spawn_ball() is called
+ball = None
 # Bricks
 bricks = pygame.sprite.Group()
 for r in range(BRICK_ROWS):
@@ -79,11 +78,11 @@ for r in range(BRICK_ROWS):
         brick.set_pos((brick_x, brick_y))
         bricks.add(brick)
 sprites.add(bricks)
-
 # Object to keep track of score
 score_manager = ScoreManager(5)
 
 # MAIN LOOP ====================================================================
+# Game continues as long as this is True
 running = True
 while running:
     # Event Loop ---------------------------------------------------------------
@@ -92,44 +91,53 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Input Handlers -----------------------------------------------------------
-    # TODO: when starting or after dying, start game w/ click
+    # Keys
     keys = pygame.key.get_pressed()
     # Quit on ESC
     if keys[pygame.K_ESCAPE]:
         running = False
-    # Paddle controls (mouse)
+    # Mouse
+    mouse_buttons = pygame.mouse.get_pressed(3)
+    # Spawn ball on click if not already in play
+    if ball is None and mouse_buttons[0]:
+        ball = Ball(BALL_COLOR, BALL_SIZE, BALL_STARTING_POS, BALL_STARTING_DIRECTION, BALL_SPEED,
+                    min_y=PLAYFIELD_TOP_Y)
+        sprites.add(ball)
+    # Paddle controls (mouse position)
     paddle.set_center_x(pygame.mouse.get_pos()[0])
     # Game Logic ---------------------------------------------------------------
-    # Update ball position, determine if ball hit bottom
-    lose_life = ball.update()
-    if lose_life:
-        score_manager.lose_life()
-        # TODO: move ball to starting position after pause
-    # Paddle/ball collision
-    if pygame.sprite.collide_mask(ball, paddle):
-        diff = paddle.rect.centerx - ball.rect.centerx
-        ball.h_bounce(paddle.rect.y - ball.size - 1, diff)
-    # Brick/ball collision
-    for brick in pygame.sprite.spritecollide(ball, bricks, False):
-        ball.handle_brick_collision(brick.rect)
-        score_manager.add_points()
-        brick.kill()
-    if len(bricks) == 0:
-        win_text = GAME_OVER_FONT.render("STAGE CLEAR", 1, UI_COLOR)
-        # TODO: centering?
-        screen.blit(win_text, (200, 300))
-        pygame.display.flip()
-        pygame.time.wait(3000)
-        # TODO: reset game instead
-        running = False
+    if ball is not None:
+        # Update ball position, determine if ball hit bottom
+        lose_life = ball.update()
+        # Paddle/ball collision
+        if pygame.sprite.collide_mask(ball, paddle):
+            diff = paddle.rect.centerx - ball.rect.centerx
+            ball.h_bounce(paddle.rect.y - ball.size - 1, diff)
+        # Brick/ball collision
+        for brick in pygame.sprite.spritecollide(ball, bricks, False):
+            ball.handle_brick_collision(brick.rect)
+            score_manager.add_points()
+            brick.kill()
+        if len(bricks) == 0:
+            win_text = GAME_OVER_FONT.render('STAGE CLEAR', True, UI_COLOR)
+            # TODO: centering?
+            screen.blit(win_text, (200, 300))
+            pygame.display.flip()
+            pygame.time.wait(3000)
+            # TODO: reset game instead
+            running = False
+        if lose_life:
+            ball.kill()
+            ball = None
+            score_manager.lose_life()
 
     # Drawing ------------------------------------------------------------------
     # Background
     screen.fill(BG_COLOR)
     # UI border and text
     pygame.draw.line(screen, UI_COLOR, [0, UI_LINE_Y], [WINDOW_WIDTH, UI_LINE_Y], UI_LINE_STROKE)
-    score_text = UI_FONT.render(f'Score: {score_manager.score}', 1, UI_COLOR)
-    lives_text = UI_FONT.render(f'Lives: {score_manager.lives}', 1, UI_COLOR)
+    score_text = UI_FONT.render(f'Score: {score_manager.score}', True, UI_COLOR)
+    lives_text = UI_FONT.render(f'Lives: {score_manager.lives}', True, UI_COLOR)
     screen.blit(score_text, UI_SCORE_POS)
     screen.blit(lives_text, UI_LIVES_POS)
     # Draw sprites
@@ -140,7 +148,7 @@ while running:
     # Handle game over
     if score_manager.lives <= 0:
         # Show game over then exit
-        game_over_text = GAME_OVER_FONT.render('GAME OVER', 1, UI_COLOR)
+        game_over_text = GAME_OVER_FONT.render('GAME OVER', True, UI_COLOR)
         # TODO positioning?
         screen.blit(game_over_text, (250, 300))
         pygame.display.flip()
